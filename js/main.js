d56fb88bca9a454d02ff4610499c606892703722
Igor Seleznov and Justin Carothers <igor@>
2016-06-02 10:41:02 -0700

    testing git amend

--- js/main.js
+++ js/main.js
@@ -1,15 +1,83 @@
-
-
-
 $(document).ready(function() {
+  $("button").click(function(event) {
+    event.preventDefault();
 
-  var age = parseInt(prompt("How old arte you"));
+    var gender = $("input:radio[name=gender]:checked").val();
+    var hair = $("input:radio[name=hair]:checked").val();
+    var age = parseInt($("#age").val());
+    var height = $("#height").val();
 
-  if (age >= 18) {
-    $("#intro").hide();
-    $('#eighteen').show();
-  } else {
-    $('#notEighteen').show();
-  };
+
+    if (gender === 'male') {
+      $(".mainPage").hide();
+      $(".malePage").show();
+
+    } else if (hair === 'blonde' && age > 50 || height === 'tall') {
+
+        $(".mainPage").hide();
+        $(".yourDate").show();
+        $(".yourDate").prepend("<h1>Scarlett</h1>");
+        alert("You chose an older blonde lady with long legs.");
+      } else {
+        alert ("Please enter age")
+      }
+// alert (gender + hair + age + height);
+
+
 });
-  // $(".NO").click(function(){
+});
+
+
+
+
+// $(document).ready(function() {
+//
+//   $(".col-sm-4").click(function(){
+//
+//    var choice = $(this).attr('id'); // or var clickedBtnID = this.id
+//    if (choice === 'turtle') {
+//      $("#main").hide();
+//      $(".turtlePage").show();
+//    }
+//    else if (choice === 'snake') {
+//      $("#main").hide();
+//      $(".snakePage").show();
+//    }
+//    else if (choice === 'insect')
+//    {
+//      $("#main").hide();
+//      $(".insectPage").show();
+//    }
+//    else {
+//      return;
+//    }
+//  });
+// });
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// //
+// //
+// //
+// // $(document).ready(function() {
+// //
+// //   var age = parseInt(prompt("How old arte you"));
+// //
+// //   if (age >= 18) {
+// //     $("#intro").hide();
+// //     $('#eighteen').show();
+// //   } else {
+// //     $('#notEighteen').show();
+// //   };
+// // });
+// //   // $(".NO").click(function(){
